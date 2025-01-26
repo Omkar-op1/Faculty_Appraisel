184fa0fcbd5d27ae3e894c406850ada762f0e76a
omkar dhatrak <ordhatrak855@gmail.com>
2025-01-26 18:25:03 +0530

    h

--- Backend/Module/server.js
+++ Backend/Module/server.js
@@ -20,7 +20,7 @@ const erp=require('./erpapi')
 
 app.use(cookieParser());
 const corsOptions = {
-  origin: 'http://127.0.0.1:5500', // specify your front-end origin
+  // origin: 'http://127.0.0.1:5500', // specify your front-end origin
   credentials: true, // allow credentials (cookies, HTTP authentication)
 }
 
